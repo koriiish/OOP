c4d0182f6f6456770d047d761943d2b7f99ee947
Karrie <karinad000@gmail.com>
2023-12-14 16:14:40 +0300

    add hw to 1412

---
+++ HW5(L7to1412)/HW5(L7to1412)/Subiect.swift
@@ -0,0 +1,12 @@
+//
+//  Subiect.swift
+//  HW5(L7to1412)
+//
+//  Created by Карина Дьячина on 14.12.23.
+//
+
+import Foundation
+struct Subject {
+    private(set) var subjectName: String
+    private(set) var grade: String
+}
